747b9eb3553a58fde1d0141bf91a05d8501fc79a
raselmandol <130071257+raselmandol@users.noreply.github.com>
2026-02-10 16:08:06 +0000

    Deploying to gh-pages from @ raselmandol/raselmandol.github.io@fbc0db55fdfb60ff0ebb522d84b4d0ddd7b41128 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -191,6 +191,13 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qr4FDowAAAAJ", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
